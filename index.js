94f2fab4a81b87e2fc8d59a3ef86666db474f68f
“Raffaella <raffaella.florissi@gmail.com>
2024-04-29 21:53:37 +0100

    solved lab

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b)=> {
+      return a-b;
+    });
+    this.length++;
+  };
 
-  get(pos) {}
+  get(pos) {
+    if(pos < 0 || pos >= this.items.length){
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    };
+  };
 
-  max() {}
+  max() {
+    let max = Math.max(...this.items);
+    if(this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return max;
+  }
 
-  min() {}
+  min() {
+    let min = Math.min(...this.items);
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return min;
+  }
 
-  sum() {}
+  sum() {
+    let sum=0;
+    this.items.forEach((element)=>{
+      sum += element;
+    })
+
+    if(this.length === 0){
+      return 0;
+    }
+    return sum;
+  }
 
   avg() {}
 }
